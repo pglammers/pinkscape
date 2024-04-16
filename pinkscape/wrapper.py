b545f1618e43d65c94c8482f75c7651e7801bafa
Piet Lammers <email@pietlammers.com>
2024-04-16 21:43:22 +0200

    add wrapper methods

--- pinkscape/wrapper.py
+++ pinkscape/wrapper.py
@@ -11,10 +11,41 @@ class SVG:
             self.et = ET.parse(StringIO(empty_svg_string))
 
     def __getitem__(self, id):
-        return self.et.findall(f""".//*[@id="{id}"]""")[0]
+        try:
+            return self.et.findall(f""".//*[@id="{id}"]""")[0]
+        except:
+            raise Exception(f'Element with id "{id}" not found in tree {self}')
 
-    def append_to(self, id, element):
-        self[id].append(element)
+    def add_layer(self, id, label):
+        element = ET.Element(
+            "svg:g",
+            attrib={
+                "inkscape:label": label,
+                "inkscape:groupmode": "layer",
+                "id": id,
+            },
+        )
+        self.et.getroot().append(element)
+
+    def add_group(self, insert_id, group_id, group_label=None):
+        attrib = {
+            "id": group_id,
+        }
+        if group_label is not None:
+            attrib["inkscape:label"] = group_label
+        element = ET.Element("svg:g", attrib=attrib)
+        self[insert_id].append(element)
+
+    def element_remove(self, id):
+        parent_map = {c: p for p in self.et.iter() for c in p}
+        child = self[id]
+        parent = parent_map[child]
+        parent.remove(child)
+
+    def element_vacate(self, id):
+        parent = self[id]
+        for child in parent:
+            parent.remove(child)
 
     def write_to(self, filename):
         ET.indent(self.et, space=4 * " ", level=0)
