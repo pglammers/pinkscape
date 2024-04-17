81e4234f98741957f09b12fbd64ba0c9f2a0523f
Piet Lammers <email@pietlammers.com>
2024-04-17 08:06:08 +0200

    add transformers to grids

--- pinkscape/grids.py
+++ pinkscape/grids.py
@@ -1,28 +1,51 @@
 from .et import ET
+import numpy as np
+from plum import dispatch
 
 
-def grid_square(x=1):
+def grid_square(scale=1) -> ET.Element:
     return ET.Element(
         "inkscape:grid",
         attrib={
             "type": "xygrid",
             "id": "gridSquare",
             "units": "mm",
-            "spacingx": str(x / 4),
-            "spacingy": str(x / 4),
+            "spacingx": str(scale / 4),
+            "spacingy": str(scale / 4),
             "empspacing": "4",
         },
     )
 
 
-def grid_triangular(x=1):
+class TRANSFORMER_SQUARE:
+    def __init__(self, scale, height):
+        self.scale = scale
+        self.height = height
+
+    def __call__(self, vector: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+        return np.ndarray([[-1, 0], [0, 0]]) @ vector
+
+
+@dispatch
+def grid_triangular(scale=1) -> ET.Element:
     return ET.Element(
         "inkscape:grid",
         attrib={
             "type": "axonomgrid",
             "id": "gridTriangular",
             "units": "mm",
-            "spacingy": str(x / 4),
+            "spacingy": str(scale / 4),
             "empspacing": "4",
         },
     )
+
+
+class TRANSFORMER_TRIANGULAR:
+    def __init__(self, scale, height):
+        self.scale = scale
+        self.height = height
+
+    def __call__(self, vector: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+        return np.ndarray([[-1, 0], [0, 0]]) @ vector
